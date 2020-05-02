a6dbf1151c7e6fa6d33e91f552dbc1f1fa2b2cec
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2020-05-02 22:56:32 +0200

    ball and paddle drawn

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,10 +1,15 @@
 
 
-import { Brick, Field, Paddle } from './../models';
+import { Brick, Field, Paddle, Ball } from './../models';
 export class GameService {
   private timeoutRefs: any[] = [];
   private requestAninmationFrameRefs: number[] = [];
   private field: Field;
+  private ball: Ball;
+  private paddle: Paddle;
+
+  private middleBottomX: number;
+  private middleBottomY: number;
 
   constructor(
     public canvas: HTMLCanvasElement,
@@ -14,6 +19,13 @@ export class GameService {
     this.canvas.height = 700;
     this.field = new Field(this.canvas, this.ctx);
     this.field.draw();
+    this.middleBottomX = (this.canvas.width / 2);
+    this.middleBottomY = this.canvas.height / 2;
+    this.ball = new Ball(this.ctx, this.middleBottomX + 10, this.middleBottomY + 10, 10, "red");
+    this.ball.draw();
+    this.paddle = new Paddle(this.canvas, this.ctx, this.middleBottomX,this.canvas.height,  150, 20, "blue");
+    this.paddle.draw();
+
   }
   public updateModels(): void {
     this.timeoutRefs.push(requestAnimationFrame(() => {
